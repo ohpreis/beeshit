79f8080f77e930b2a4d7636ae20a2306000ab734
Oscar <ohpreis@gmail.com>
2023-07-10 11:05:51 -0500

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,11 @@
 import '@styles/globals.css'
 import Script from 'next/script'
 
-function Application({ Component, pageProps }) {
-  return 
-    (
-      <>
+export default function MyApp({ Component, pageProps }) {
+  return (
+    <>
       <Component {...pageProps} />
-      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8425699452348838" crossorigin="anonymous"></script>
-      </>
-    )
+      <Script src="https://example.com/script.js" />
+    </>
+  )
 }
-
-export default Application
