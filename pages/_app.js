e0752a2ef6cfa982483576deef70459716e51552
Oscar <ohpreis@gmail.com>
2023-07-10 11:01:20 -0500

    Update _app.js
    Added Google Adsense?

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
 import '@styles/globals.css'
+import Script from 'next/script'
 
 function Application({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return 
+    (
+      <>
+      <Component {...pageProps} />
+      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8425699452348838" crossorigin="anonymous"></script>
+      </>
+    )
 }
 
 export default Application
